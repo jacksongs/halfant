27c5db48203d821ac8a631c195a851ed0190bd3f
Jackson Gothe-Snape <jacksongs@gmail.com>
2014-09-15 21:05:25 +1000

    psql6

--- half/settings.py
+++ half/settings.py
@@ -61,7 +61,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'halfdb',
         'USER': 'postgres',
-        'PASSWORD': 'memory00',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
